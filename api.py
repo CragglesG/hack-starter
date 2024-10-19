a59883618286d03f43918c9a6e618e8dfdff8ef1
CragglesG <121510295+CragglesG@users.noreply.github.com>
2024-10-19 16:19:34 +0100

    Use Docker client for start-hack

--- api.py
+++ api.py
@@ -3,19 +3,23 @@ import uvicorn
 import os
 from dotenv import load_dotenv
 import subprocess
+import docker
 
 load_dotenv()
+
+client = docker.from_env()
 
 app = fastapi.FastAPI()
 
 @app.post("/start-hack")
 def start_hack(data: dict):
     try:
-        port = int(subprocess.run(["nest", "get_port"], capture_output=True).stdout.decode().strip().split(' ')[1])
+        port = subprocess.run(["nest", "get_port"], capture_output=True).stdout.decode().strip().split(' ')[1]
         with open("start-hack") as f:
-            subprocess.run(f.read().replace("[PORT]", str(port))
-                      .replace("[NAME]", data["user_id"] + "-" + data["project_name"])
-                      .replace("[PASSWORD]", data["password"]))
+            client.containers.run(image="lscr.io/linuxserver/code-server:latest", detach=True,
+                                  name=data["user_id"] + "-" + data["project_name"], ports={port: "8443"},
+                                  volumes={"~/.config/code-server/config.yaml": {"bind": "/config/config.yaml", "mode": "rw"}},
+                                  environment={"PASSWORD": data["password"], "TZ": "Etc/UTC", "PUID": "1000", "PGID": "1000"})
         subprocess.run(f"nest caddy add {data['project_name']}.{data['username']}.craigg.hackclub.app --proxy 0.0.0.0:{port}".split(" "))
         return {"url": f"https://{data['project_name']}.{data['username']}.craigg.hackclub.app"}
     except Exception as e:
